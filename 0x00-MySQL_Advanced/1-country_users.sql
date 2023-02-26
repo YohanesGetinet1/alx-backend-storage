dc76fd26342ddb21d33d5b466bb99427a5453e48
YohanesGetinet1 <johnget496@gmail.com>
2023-02-26 17:58:57 +0530

    Task 1 sql script

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- A table called user
+-- with attributes 
+-- id integer, not null, auto incr and primary key
+-- email string with 255 character, not null, and unique
+-- name string 255 character
+-- enumeratioin of countries US, CO AND TN
+
+create table if not exists users(
+    id int not null auto_increment primary key,
+    email varchar(255) not null unique,
+    name varchar(255) not null
+    countries enum ('US','CO','TN') default 'US' not null 
+);
