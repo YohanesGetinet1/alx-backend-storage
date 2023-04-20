90abf381db264900bbafc465de310677261485c6
Yohanes Getinet <74892375+YohanesGetinet1@users.noreply.github.com>
2023-04-20 19:37:24 +0530

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,9 @@
 -- email string with 255 character, not null, and unique
 -- name string 255 character
 
-create table if not exists users(
-    id int not null auto_increment primary key,
-    email varchar(255) not null unique,
-    name varchar(255) not null 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
