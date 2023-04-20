f90fcce8efbe859a89064195205b7441585aaa76
Yohanes Getinet <74892375+YohanesGetinet1@users.noreply.github.com>
2023-04-20 19:42:33 +0530

    Update 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,8 +2,14 @@
 --the quantity of an item after 
 --adding a new order
 
-create trigger decrease_quantity after insert on orders
-for each row update items
-set
-quantity = quantity - NEW.number
-where name = NEW.item_name;
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
